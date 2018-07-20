c0ea61462eea1063ab73854159788e69bd845a11
Wyn Price <wynprice999@gmail.com>
2018-07-20 10:02:23 +0100

    Fixed minor issue with missing model not rendering

--- src/main/java/net/dumbcode/projectnublar/client/render/model/ModelMissing.java
+++ src/main/java/net/dumbcode/projectnublar/client/render/model/ModelMissing.java
@@ -8,15 +8,16 @@ public class ModelMissing extends ModelBase {
 
     public static ModelMissing INSTANCE = new ModelMissing();
 
-    public ModelRenderer cube;
+    private ModelRenderer cube;
 
     private ModelMissing() {
         this.cube = new ModelRenderer(this, 0, 0);
-        this.cube.addBox(0F, 0F, 0F, 16, 16, 16, 0.0F);
+        this.cube.addBox(-8F, 8F, -8F, 16, 16, 16, 0.0F);
     }
 
     @Override
     public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        this.cube.render(scale);
         super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
     }
 }
