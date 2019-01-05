813c4ff934b9a4ccbfd98acdf34d6a6f5f6eaf87
Brandon Davis <neusfear@gmail.com>
2019-01-04 21:59:53 -0600

    version bump

--- src/main/java/net/dumbcode/projectnublar/server/ProjectNublar.java
+++ src/main/java/net/dumbcode/projectnublar/server/ProjectNublar.java
@@ -58,7 +58,7 @@ import java.util.Map;
 public class ProjectNublar {
     public static final String MODID = "projectnublar";
     public static final String NAME = "Project Nublar";
-    public static final String VERSION = "0.0.4";
+    public static final String VERSION = "0.0.5";
     public static final String LLIBRARY_VERSION = "1.7.15";
     public static final String DUMBLIBRARY_VERSION = "0.1.9";
 
