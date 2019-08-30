f3e9555997690839c82e298c3464a185db7082e4
Wyn-Price <wynprice999@gmail.com>
2019-08-30 03:08:44 +0100

    Fixed tesr error

--- src/main/java/net/dumbcode/projectnublar/client/render/model/DinosaurModelHandler.java
+++ src/main/java/net/dumbcode/projectnublar/client/render/model/DinosaurModelHandler.java
@@ -3,16 +3,19 @@ package net.dumbcode.projectnublar.client.render.model;
 import net.dumbcode.dumblibrary.client.component.ComponentRenderer;
 import net.dumbcode.projectnublar.client.render.blockentity.BlockEntityElectricFencePoleRenderer;
 import net.dumbcode.projectnublar.client.render.blockentity.BlockEntityElectricFenceRenderer;
+import net.dumbcode.projectnublar.client.render.blockentity.BlockEntityIncubatorRenderer;
 import net.dumbcode.projectnublar.client.render.blockentity.BlockEntitySkeletalBuilderRenderer;
 import net.dumbcode.projectnublar.client.render.entity.EntityPartRenderer;
 import net.dumbcode.projectnublar.client.render.entity.GyrosphereRenderer;
 import net.dumbcode.projectnublar.server.ProjectNublar;
 import net.dumbcode.projectnublar.server.block.entity.BlockEntityElectricFence;
 import net.dumbcode.projectnublar.server.block.entity.BlockEntityElectricFencePole;
+import net.dumbcode.projectnublar.server.block.entity.IncubatorBlockEntity;
 import net.dumbcode.projectnublar.server.block.entity.SkeletalBuilderBlockEntity;
 import net.dumbcode.projectnublar.server.entity.DinosaurEntity;
 import net.dumbcode.projectnublar.server.entity.EntityPart;
 import net.dumbcode.projectnublar.server.entity.vehicles.GyrosphereVehicle;
+import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -29,13 +32,11 @@ public class DinosaurModelHandler {
         RenderingRegistry.registerEntityRenderingHandler(DinosaurEntity.class, ComponentRenderer::new);
         RenderingRegistry.registerEntityRenderingHandler(GyrosphereVehicle.class, GyrosphereRenderer::new);
         RenderingRegistry.registerEntityRenderingHandler(EntityPart.class, EntityPartRenderer::new);
-    }
 
-    @SubscribeEvent
-    public static void onModelsBaked(ModelBakeEvent event) {
         ClientRegistry.bindTileEntitySpecialRenderer(SkeletalBuilderBlockEntity.class, new BlockEntitySkeletalBuilderRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(BlockEntityElectricFencePole.class, new BlockEntityElectricFencePoleRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(BlockEntityElectricFence.class, new BlockEntityElectricFenceRenderer());
 
     }
+
 }
