ad73d5b2df8cb419c511b0ed40e98d0dedeb9594
Wyn-Price <wynprice999@gmail.com>
2019-11-16 21:00:04 +0000

    Update StructureNetwork.java

--- src/main/java/net/dumbcode/projectnublar/server/world/structures/network/StructureNetwork.java
+++ src/main/java/net/dumbcode/projectnublar/server/world/structures/network/StructureNetwork.java
@@ -81,8 +81,7 @@ public class StructureNetwork {
                     pos = WorldUtils.getDirectTopdownBlock(world, pos.add(startingPosition)).down();
                     if(world.getBlockState(pos).getMaterial().isLiquid()) {
                         world.setBlockState(pos, BlockUtils.getBiomeDependantState(Blocks.PLANKS.getDefaultState(), biome));
-                    }
-                    if(perc < 0.6) {
+                    } else if(perc < 0.6) {
                         world.setBlockState(pos, BlockUtils.getBiomeDependantState(Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.COARSE_DIRT), biome));
                     }
                 }
