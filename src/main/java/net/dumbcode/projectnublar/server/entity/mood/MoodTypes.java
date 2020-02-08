bfbb392577b76c9949558f2b140ab1f26ddb1b8e
Wyn-Price <wynprice999@gmail.com>
2020-02-08 22:44:05 +0000

    Added happy mood affecting sleeping component

--- src/main/java/net/dumbcode/projectnublar/server/entity/mood/MoodTypes.java
+++ src/main/java/net/dumbcode/projectnublar/server/entity/mood/MoodTypes.java
@@ -2,6 +2,8 @@ package net.dumbcode.projectnublar.server.entity.mood;
 
 import net.dumbcode.dumblibrary.server.attributes.ModifiableFieldModifier;
 import net.dumbcode.dumblibrary.server.attributes.ModOp;
+import net.dumbcode.dumblibrary.server.ecs.component.EntityComponentTypes;
+import net.dumbcode.dumblibrary.server.ecs.component.impl.SleepingComponent;
 import net.dumbcode.projectnublar.server.entity.ComponentHandler;
 import net.dumbcode.projectnublar.server.entity.component.impl.MetabolismComponent;
 
@@ -11,17 +13,19 @@ public class MoodTypes {
 
     public static final MoodType STRESSFUL = MoodType.builder()
         .translationKey("projectnublar.mood.stressful")
-        .addFieldModifierCallback(ComponentHandler.METABOLISM, MetabolismComponent::getFoodRate,
-            amount -> new ModifiableFieldModifier(UUID.fromString("0f5179dc-1ad6-4123-aac9-40d0d0126d7f"), ModOp.MULTIPLY_BASE_THEN_ADD, -amount / 16F) //Adds +/- 0.25 to the food rate
+        .addFieldModifierCallback(ComponentHandler.METABOLISM, MetabolismComponent::getFoodRate, amount ->
+            new ModifiableFieldModifier(UUID.fromString("0f5179dc-1ad6-4123-aac9-40d0d0126d7f"), ModOp.MULTIPLY_BASE_THEN_ADD, -amount / 16F) //Adds +/- 0.25 to the food rate
         )
-        .addFieldModifierCallback(ComponentHandler.METABOLISM, MetabolismComponent::getWaterRate,
-            amount -> new ModifiableFieldModifier(UUID.fromString("05dd8f00-ac20-4890-8ae2-812da9bc22a7"), ModOp.MULTIPLY_BASE_THEN_ADD, -amount / 16F) //Adds +/- 0.25 to the water rate
+        .addFieldModifierCallback(ComponentHandler.METABOLISM, MetabolismComponent::getWaterRate, amount ->
+            new ModifiableFieldModifier(UUID.fromString("05dd8f00-ac20-4890-8ae2-812da9bc22a7"), ModOp.MULTIPLY_BASE_THEN_ADD, -amount / 16F) //Adds +/- 0.25 to the water rate
         )
         .build();
 
     public static final MoodType HAPPY = MoodType.builder()
         .translationKey("projectnublar.mood.happy")
-
+        .addFieldModifierCallback(EntityComponentTypes.SLEEPING, SleepingComponent::getTirednessChanceConstant, amount ->
+            new ModifiableFieldModifier(UUID.fromString("d7ad52a0-4421-4b32-85d4-b1201d4c012d"), ModOp.MULTIPLY_BASE_THEN_ADD, -amount / 8F)
+        )
         .build();
 
 
