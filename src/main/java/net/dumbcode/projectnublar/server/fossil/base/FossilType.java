73e93e20904b61d045f8583b58bb3cc8e6ec3770
vaylor27 <godisagodlol@gmail.com>
2023-09-17 20:46:43 -0400

    add FossilType

---
+++ src/main/java/net/dumbcode/projectnublar/server/fossil/base/FossilType.java
@@ -0,0 +1,18 @@
+package net.dumbcode.projectnublar.server.fossil.base;
+
+public enum FossilType {
+    PETRIFIED(0.8F),
+    MOLD_CAST(0.6F),
+    CARBON_FILM(0.7F),
+    TRACE(0.5F),
+    PRESERVED_REMAINS(0.9F),
+    COMPRESSION(0.4F),
+    IMPRESSION(0.3F),
+    PSEUDO(0.2F);
+
+    private final float DNAMultiplier;
+
+    FossilType(float DNAMultiplier) {
+        this.DNAMultiplier = DNAMultiplier;
+    }
+}
